04db8180514c8e1539ad99df9e677ea87781c770
baba_s <shotababa1206@gmail.com>
2019-05-01 16:28:25 +0900

    fist commit

---
+++ Editor/Example.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UPMTest
+{
+	public static class Example
+	{
+		[MenuItem( "Tools/Hoge" )]
+		private static void Hoge()
+		{
+			Debug.Log( "ピカチュウ" );
+		}
+	}
+}
